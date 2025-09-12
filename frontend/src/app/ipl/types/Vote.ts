4a83081924a012f935803f985b60c6dab2b04836
Harshada Jagtap <127108103+harshadacsgithub@users.noreply.github.com>
2025-09-12 09:15:54 +0530

    Update Vote.ts

--- frontend/src/app/ipl/types/Vote.ts
+++ frontend/src/app/ipl/types/Vote.ts
@@ -1,4 +1,37 @@
 
+// import { Cricketer } from "./Cricketer";
+// import { Team } from "./Team";
+
 export class Vote {
 
+    voteId: number;
+    email: string;
+    category: string;
+    // cricketer: Cricketer;
+    // team: Team;
+    cricketerId:number;
+    teamId:number
+
+    constructor(
+        voteId: number,
+        email: string,
+        category: string,
+        // cricketer: Cricketer,
+        // team: Team
+        cricketerId:number,
+        teamId:number
+    ) {
+        this.voteId = voteId;
+        this.email = email;
+        this.category = category;
+        // this.cricketer = cricketer;
+        // this.team = team;
+        this.cricketerId=cricketerId;
+        this.teamId=teamId;
+    }
+
+    displayInfo() {
+        console.log(`Vote ID: ${this.voteId}`);
+        console.log(`Email: ${this.email}`);
+    }
 }
